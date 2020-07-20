9a6999315bf4da09155f0e218ad55babb4a77d1f
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2020-07-20 15:59:06 +0200

    Create method to check broadcast connection for Televise

--- cu-interfaces.LIB/Klassen/Televisie.cs
+++ cu-interfaces.LIB/Klassen/Televisie.cs
@@ -7,7 +7,7 @@ using cu_interfaces.LIB.Interfaces;
 
 namespace cu_interfaces.LIB.Klassen
 {
-    public class Televisie : ElektrischToestel, IPower,IVolume
+    public class Televisie : ElektrischToestel, IPower,IVolume,ICheckBroadCastConnection
     {
         public bool IsOn { get; set; }
         public int CurrentVolume { get; private set; } = 50;
@@ -45,6 +45,41 @@ namespace cu_interfaces.LIB.Klassen
             {
                 CurrentVolume = 0;
             }
+        }
+
+        static Random rnd = new Random();
+
+        private bool IsCoaxCableConnected()
+        {
+            int trueOrFalse = rnd.Next(2);
+            return Convert.ToBoolean(trueOrFalse);
+        }
+
+        private bool IsSignalAvailable() 
+        {
+            int trueOfFalse = rnd.Next(2);
+            return Convert.ToBoolean(trueOfFalse);
+        }
+        
+        private bool IsSignalStrengthOK()
+        {
+            int trueOrFalse = rnd.Next(20);
+            return Convert.ToBoolean(trueOrFalse);
+        }
+
+        public string CheckBroadCastConnection()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"======Testing {this.GetType().Name} {LivingRoom}======");
+            stringBuilder.AppendLine("Is COAX connected? Checking connection.\nPlease wait.");
+            stringBuilder.AppendLine($"COAX test returns {IsCoaxCableConnected()}{Environment.NewLine}");
+
+            stringBuilder.AppendLine("Is signal available? Checking sigal.\nPlease wait.");
+            stringBuilder.AppendLine($"Signal test return {IsSignalAvailable()}");
+            stringBuilder.AppendLine($"Signal strenght test returns {IsSignalStrengthOK()}");
+            stringBuilder.AppendLine($"===End of test {this.GetType().Name}{LivingRoom}==={Environment.NewLine}");
+
+            return stringBuilder.ToString();
         }
     }
 }
