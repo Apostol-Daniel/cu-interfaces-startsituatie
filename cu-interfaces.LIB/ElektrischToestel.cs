5ef98b917083448104ebfb1faf619e8c63b88ca2
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2020-07-20 14:36:15 +0200

    Create Televise class and add constructor to it in order to link it to th ElektrischToestel class

---
+++ cu-interfaces.LIB/ElektrischToestel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cu_interfaces.LIB
+{
+    public abstract class ElektrischToestel
+    {
+        public string LivingRoom { get; private set; }
+
+        public ElektrischToestel(string livingRoom)
+        {
+            LivingRoom = livingRoom;
+        }
+    }
+}
