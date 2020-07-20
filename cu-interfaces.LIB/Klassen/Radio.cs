a7e2467f52e63961ac1a47ce96de2d1cafa2b5cb
Apostol Daniel <marian-daniel.apostol@student.howest.be>
2020-07-20 15:01:34 +0200

    Make Radio implement IPower interface

--- cu-interfaces.LIB/Klassen/Radio.cs
+++ cu-interfaces.LIB/Klassen/Radio.cs
@@ -1,16 +1,32 @@
-﻿using System;
+﻿using cu_interfaces.LIB.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace cu_interfaces.LIB.Klassen
 {
-    class Radio : ElektrischToestel
+    public class Radio : ElektrischToestel ,IPower
     {
+
+        public bool IsOn { get; set; }
         public Radio(string livingRoom) : base(livingRoom)
         {
 
+        }
+
+        public string PowerOff()
+        {
+            IsOn = false;
+            return $"Radio {LivingRoom} is uit";
+        }
+
+        public string PowerOn()
+        {
+            IsOn = true;
+            return $"Radio {LivingRoom} is aan";
         }
     }
 }
